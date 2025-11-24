a40f5e0cb4acd8edf65d3f7a390fa2913ba1e913
Harsh-Sharma-10 <harshsharmasharma943@gmail.com>
2025-11-24 19:43:29 +0530

    Documnetation purpose

--- Patient-Services/src/main/java/com/pm/patientservices/controller/AdminController.java
+++ Patient-Services/src/main/java/com/pm/patientservices/controller/AdminController.java
@@ -3,6 +3,8 @@ package com.pm.patientservices.controller;
 
 import com.pm.patientservices.Model.DeletedPatient;
 import com.pm.patientservices.Services.Logs_PatientServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/logscontroller")
+@Tag(name = "PATIENTS MANAGEMENT SYSTEMS",description = "API for DELETED/CHECKED OUT PATIENTS")
 public class AdminController {
 
     private final Logs_PatientServices  logs_PatientServices;
@@ -20,10 +23,12 @@ public class AdminController {
     }
 
     @GetMapping("/deletedpatients")
+    @Operation(summary = "To get the list of deleted patients")
     public ResponseEntity<List<DeletedPatient>> getdeletedpatients(){
         return new ResponseEntity<>(logs_PatientServices.get_records(), HttpStatus.OK);
     }
     @GetMapping("/deletedpatients/{id}")
+    @Operation(summary = "To get record of a deleted person")
     public ResponseEntity<?>getdeletedpatient(@PathVariable UUID id) {
         DeletedPatient deletedPatient = logs_PatientServices.get_record(id);
         if (deletedPatient != null) {
@@ -32,6 +37,7 @@ public class AdminController {
         return new ResponseEntity<>("PATIENT IS NOT CHECKED OUT YET",HttpStatus.CONFLICT);
     }
     @DeleteMapping("/deletepermantly/{id}")
+    @Operation(summary = "To delete the patient's record permanently")
     public ResponseEntity<String> deletepermantly(@PathVariable UUID id){
         logs_PatientServices.Deletepermanently(id);
           return new ResponseEntity<>("DELETED PERMANTLY",HttpStatus.NO_CONTENT);
